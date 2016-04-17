a7d9d5afb8a315da09c34fb0f2b15f0bfc7da613
James Newman <jamesmnewman1@gmail.com>
2016-04-16 17:31:20 -0700

    angular mail reader

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var app = angular.module("myApp", []);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
+	})
+	.when('/settings', {
+		templateUrl: 'templates/settings.html',
+		controller: 'SettingsController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
+
+app.controller('HomeController', function($scope) {
+
+});
+
+app.controller('SettingsController', function($scope) {
+	$scope.settings = {
+		name: 'James',
+		email: 'james@mail.com',
+		age: 24
+	}
+	$scope.updateSettings = function() {
+		console.log('function called');
+	}
+});
